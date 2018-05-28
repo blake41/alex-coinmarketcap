3cbb7cb519108cda3955e2a3930c8abe8dcade72
Blake Johnson <blake41@gmail.com>
2018-05-28 13:06:14 -0300

    working sorta

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -9,13 +9,6 @@ class Scraper
     @url = url
   end
 
-  def run(date = 20130428)
-    page = bot.get("#{url}/all/views/all/")
-    page.links_with(:href => /currencies\/\w+\/\z/).each do |link|
-      get_data_for_currency(link, date, url)
-    end
-  end
-
   def update_data
     if get_last_day_run < Date.today
       date_to_run = get_last_day_run + 1.day
@@ -26,28 +19,42 @@ class Scraper
     run(date_to_run.strftime("%Y%m%d"))
   end
 
-  def add_new_coins(date = 20130428)
-    Currency.last.destroy
+  def run(date)
     page = bot.get("#{url}/all/views/all/")
-    page.links_with(:href => /currencies\/\w+\/\z/).uniq {|link| link.href}.each do |link|
-      next if Currency.find_by_ticker(link.text)
-      get_data_for_currency(link, date, url)
+    page.links_with(:href => /currencies\/\w+-?\w*-?\w*\/\z/).uniq {|link| link.href}.each do |link|
+      get_data_for_currency(link, date)
     end
   end
 
-  def get_data_for_currency(link, start_date, url)
+  def add_new_coins(date = 20130428)
+    # Currency.last.destroy if Currency.count > 0
+    page = bot.get("#{url}/all/views/all/")
+    page.links_with(:href => /currencies\/\w+-?\w*-?\w*\/\z/).uniq {|link| link.href}.each do |link|
+      next if Currency.find_by_ticker(link.text)
+      get_data_for_currency(link, date)
+    end
+  end
+
+  def get_data_for_currency(link, start_date)
     currency_page = mechanize_request {link.click}
     uri = link.uri
-    currency_name = uri.path.match(/\/\w+\/(\w+)/)[1]
+    currency_name = uri.path.match(/\/\w+\/(\w+-?*\w*-?\w*)/)[1]
     today = Date.today.strftime("%m%d")
     currency_url = "#{url}/currencies/#{currency_name}/historical-data/?start=#{start_date}&end=2018#{today}"
     data = mechanize_request {bot.get(currency_url)}
     return if data.nil?
     ticker = data.css(".hidden-xs.bold").text.gsub("(", "").gsub(")","")
+    if currency_name == "batcoin"
+      ticker = "BTA"
+    end
     c = Currency.find_or_create_by(:name => currency_name, :ticker => ticker)
     data.css('tbody')[0].children.select(&:element?).each do |row|
       cells = row.children.select(&:element?)
+      begin
       row = c.historical_datum.new(:date => cells[0].children[0].text, :open => cells[1].children[0].text, :high => cells[2].children[0].text, :low => cells[3].children[0].text, :close => cells[4].children[0].text, :volume => cells[5].children[0].text.gsub(",", "").to_i, :market_cap => cells[6].children[0].text.gsub(",", "").to_i)
+    rescue
+      next
+    end
       if row.valid?
         row.save
       end
@@ -55,7 +62,7 @@ class Scraper
   end
 
   def get_last_day_run
-    Currency.find_by_name(:bitcoin).first.date
+    HistoricalDatum.where(:currency_id => 171).order("date desc").first.date
   end
 
   def mechanize_request
